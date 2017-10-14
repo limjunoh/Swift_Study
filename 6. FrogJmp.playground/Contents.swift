fa646dfa103fe314e223ccb2bc80ebcf26e53df6
임준오 <kjo05067@gmail.com>
2017-10-14 23:16:30 +0900

    Add extra solution
    solution 2 :+1:

--- 6. FrogJmp.playground/Contents.swift
+++ 6. FrogJmp.playground/Contents.swift
@@ -4,4 +4,11 @@ public func solution(_ X : Int, _ Y : Int, _ D : Int) -> Int {
     return Int(answer.rounded(.up))
 }
 
+public func solution2(_ X : Int, _ Y : Int, _ D : Int) -> Int {
+    // write your code in Swift 3.0 (Linux)
+    return (Y-X) % D > 0 ? (Y-X)/D + 1 : (Y-X)/D
+}
+
+
 print(solution(10, 85, 30))
+print(solution2(10, 85, 30))
