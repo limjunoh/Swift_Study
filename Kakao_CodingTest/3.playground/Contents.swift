2b4277e8e4c61816c242e172f06a3eb9b93f7298
임준오 <kjo05067@gmail.com>
2017-10-27 12:12:17 +0900

    3
    3

--- Kakao_CodingTest/3.playground/Contents.swift
+++ Kakao_CodingTest/3.playground/Contents.swift
@@ -1,5 +1,13 @@
-//: Playground - noun: a place where people can play
+import Foundation
 
-import UIKit
+public func solution(_ cacheSize: Int, _ cities:[String]) -> Int {
+    var caches: [String] = []
+    
+}
 
-var str = "Hello, playground"
+/*
+ 캐시 크기(cacheSize)와 도시이름 배열(cities)을 입력받는다.
+ cacheSize는 정수이며, 범위는 0 ≦ cacheSize ≦ 30 이다.
+ cities는 도시 이름으로 이뤄진 문자열 배열로, 최대 도시 수는 100,000개이다.
+ 각 도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자로 구성되며, 대소문자 구분을 하지 않는다. 도시 이름은 최대 20자로 이루어져 있다.
+ */
